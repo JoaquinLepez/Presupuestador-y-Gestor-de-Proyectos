198f3a970db74e33855f1ed8e87af54a1f556b5f
Joaquin Lepez <joacolepez@gmail.com>
2024-06-07 21:57:30 -0300

    Add User and UserData models and their tests #4 #5 #6 #8

--- app/models/user.py
+++ app/models/user.py
@@ -1,4 +1,20 @@
 from app import db
 
 class User(db.Model):
-    pass
+    __tablename__ = 'users'
+    # Atributos propios
+    id: int = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username: str = db.Column(db.String(80), unique=True, nullable=False)
+    password: str = db.Column(db.String(255), nullable=False)
+    email: str = db.Column(db.String(120), unique=True, nullable=False)
+
+    # Relaciones con otras tablas
+    # Userdata 1:1
+    data = db.relationship('UserData', uselist=False, back_populates='user')
+
+    # Tasks N:M
+    # Teams N:M
+
+
+
+
---
+++ app/services/security.py
@@ -0,0 +1,11 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class Security:
+
+    @staticmethod
+    def generate_password(text):
+        return generate_password_hash(text)
+    
+    @staticmethod
+    def check_password(text1, text2):
+        return check_password_hash(text1, text2)
