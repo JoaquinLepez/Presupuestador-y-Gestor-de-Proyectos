e3b418c09c737212e8483bd12177d193d2234b0e
Joaquin Lepez <joacolepez@gmail.com>
2024-07-17 22:32:21 -0300

    Update app.py and add boot.ps1 #2 #7

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,21 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import os
 from app.config import config
 from .resources import main as main_blueprint
 
 db = SQLAlchemy()
 migrate = Migrate()
 
-def create_app(config_name):
+def create_app():
+    app_context = os.getenv("FLASK_CONTEXT")
+    print(f"app_context: {app_context}")
+
     app = Flask(__name__)
-    app.config.from_object(config[config_name])
-    
+    configuration = config[app_context if app_context else 'development']
+    app.config.from_object(configuration)
+
     db.init_app(app)
     migrate.init_app(app, db)
 
