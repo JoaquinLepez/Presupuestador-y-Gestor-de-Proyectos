53dca5e1161fa99290af7736b35261df2eb492ae
Joaquin Lepez <joacolepez@gmail.com>
2024-07-17 22:20:23 -0300

    Modify creator.py to app.py and update create_app()

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from app.config import config
 from .resources import main as main_blueprint
 
 db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
+    
     db.init_app(app)
+    migrate.init_app(app, db)
 
     app.register_blueprint(main_blueprint)
 
