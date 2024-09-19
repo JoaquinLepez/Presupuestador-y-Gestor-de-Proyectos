077f9ad118082985b98c216623868d7214165351
Joaquin Lepez <joacolepez@gmail.com>
2024-09-19 18:45:16 -0300

    Update boot.ps1

--- boot.ps1
+++ boot.ps1
@@ -2,6 +2,11 @@ param (
     [string]$VAR_CONTEXT = "development"
 )
 
+# Activar el entorno virtual
+& "venv/Scripts/Activate"
+Write-Output "Environment activated"
+
+# Controlar el contexto del entorno
 if ($VAR_CONTEXT -eq "production") {
     $VAR_CONTEXT = "production"
 } elseif ($VAR_CONTEXT -eq "testing") {
@@ -12,4 +17,6 @@ if ($VAR_CONTEXT -eq "production") {
 
 Write-Output "Environment set to: $VAR_CONTEXT"
 $env:FLASK_CONTEXT = $VAR_CONTEXT
+
+# Iniciar la aplicación Flask
 python app.py
